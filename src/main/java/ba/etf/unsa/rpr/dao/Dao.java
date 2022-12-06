3f5cd3b03c51f467dc3e50e4acf229b170f89fb6
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-06 19:12:40 +0100

    Added main DAO interface

--- src/main/java/ba/etf/unsa/rpr/dao/Dao.java
+++ src/main/java/ba/etf/unsa/rpr/dao/Dao.java
@@ -8,6 +8,41 @@ import java.util.List;
  */
 
 public interface Dao<T> {
+    /**
+     * Get item from database based on ID
+     * @param id
+     * @return
+     */
+    T getById(int id);
+
+
+    /**
+     * Insert given item in database
+     * @param item
+     * @return inserted item
+     */
+    T insert(T item);
+
+    /**
+     * Update item based on id
+     * @param item
+     * @return updated item
+     */
+    T update(T item);
+
+    /**
+     * Delete item from database based on ID
+     * @param id
+     */
+    void delete(int id);
+
+
+    /**
+     * Return all items from database in a list
+     * @return
+     */
+    List<T> getAll();
+
 
 
 
