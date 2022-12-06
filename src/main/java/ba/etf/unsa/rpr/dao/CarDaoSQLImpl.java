26343ca28fd934ea6fe80b45e53023b9f3b8b045
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-06 21:04:11 +0100

    Implemented constructor car impl

--- src/main/java/ba/etf/unsa/rpr/dao/CarDaoSQLImpl.java
+++ src/main/java/ba/etf/unsa/rpr/dao/CarDaoSQLImpl.java
@@ -3,17 +3,29 @@ package ba.etf.unsa.rpr.dao;
 import ba.etf.unsa.rpr.domain.Cars;
 import ba.etf.unsa.rpr.domain.Category;
 
+import java.sql.*;
 import java.util.List;
 
 public class CarDaoSQLImpl implements CarDao {
+    private Connection conn;
+    public CarDaoSQLImpl()
+    {
+        try{
+            this.conn = DriverManager.getConnection("jdbc://sql7.freemysqlhosting.net:3306/abc","abc","abc");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public List<Cars> seachByCategory(Category category) {
         return null;
     }
 
+
+
     @Override
     public Cars getById(int id) {
-        return null;
     }
 
     @Override
