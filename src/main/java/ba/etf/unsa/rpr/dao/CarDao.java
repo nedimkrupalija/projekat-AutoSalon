f16e6524c744dfbdb0f044fcdc788da33757f07e
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-06 19:41:30 +0100

    Added methods cardao

--- src/main/java/ba/etf/unsa/rpr/dao/CarDao.java
+++ src/main/java/ba/etf/unsa/rpr/dao/CarDao.java
@@ -1,12 +1,23 @@
 package ba.etf.unsa.rpr.dao;
 
+import ba.etf.unsa.rpr.domain.Cars;
+import ba.etf.unsa.rpr.domain.Category;
+
+import java.util.List;
+
 /**
  * Dao interface for cars
  * @author Nedim Krupalija
  */
 
-public interface CarDao {
+public interface CarDao extends Dao<Cars> {
 
+    /**
+     * Method for searching for cars of certain category
+     * @param category
+     * @return list of cars of given category
+     */
+    List<Cars> seachByCategory(Category category);
 
 
 
