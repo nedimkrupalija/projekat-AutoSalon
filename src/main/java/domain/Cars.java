bc729a7496458229487f9dccb22765e20e9a1729
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-06 17:52:52 +0100

    Cars pojo

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,7 +1,95 @@
 package domain;
 
+import java.util.Objects;
+
 public class Cars {
+    private int id;
+    private String make;
+    private String model;
+    private String year;
+    private Category category;
+    private String color;
+    private int hP;
 
+    @Override
+    public String toString() {
+        return "Cars{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year='" + year + '\'' +
+                ", category=" + category +
+                ", color='" + color + '\'' +
+                ", hP=" + hP +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars = (Cars) o;
+        return id == cars.id;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int gethP() {
+        return hP;
+    }
+
+    public void sethP(int hP) {
+        this.hP = hP;
+    }
 }
