30162ea13d45877caed24745d0615000a7ce3691
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-07 20:55:14 +0100

    Implemented constructor

--- src/main/java/ba/etf/unsa/rpr/dao/CategoryDAOSQlImpl.java
+++ src/main/java/ba/etf/unsa/rpr/dao/CategoryDAOSQlImpl.java
@@ -2,9 +2,33 @@ package ba.etf.unsa.rpr.dao;
 
 import ba.etf.unsa.rpr.domain.Category;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 public class CategoryDAOSQlImpl implements CategoryDao{
+
+    private Connection conn ;
+
+    /**
+     * Default constructor for CategoryDao implementation, makes connection to database
+     * Parameters hidden
+     */
+    public CategoryDAOSQlImpl(){
+        try {
+            FileReader reader = new FileReader("properties.db");
+            Properties property = new Properties();
+            property.load(reader);
+            this.conn = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/"+property.getProperty("username"),property.getProperty("username"),property.getProperty("password"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public Category getById(int id) {
         return null;
