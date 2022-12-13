4fb1b025835de63038806ebf7dcbb1ba86692707
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-13 21:41:11 +0100

    small update and scene switching

--- src/main/java/ba/etf/unsa/rpr/controller/loginController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/loginController.java
@@ -4,10 +4,17 @@ import ba.etf.unsa.rpr.dao.SalesmanDao;
 import ba.etf.unsa.rpr.dao.SalesmanDaoSQLImpl;
 import ba.etf.unsa.rpr.domain.Salesman;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 
 public class loginController {
@@ -25,7 +32,7 @@ public class loginController {
      */
     private void setWrongPass(){
         passwordField.getStyleClass().removeAll("fieldWrong");
-        usernameField.getStyleClass().add("fieldWrong");
+        passwordField.getStyleClass().add("fieldWrong");
     }
 
     public TextField usernameField;
@@ -35,7 +42,7 @@ public class loginController {
      * Go to admin panel if salesman is chosen
      * @param actionEvent btn click
      */
-    public void loginClick(ActionEvent actionEvent) {
+    public void loginClick(ActionEvent actionEvent) throws IOException {
         if(passwordField.getText().isEmpty()){
             setWrongPass();
             System.out.println("Prazno polje za password.");
@@ -54,7 +61,16 @@ public class loginController {
             }
             else{
                 System.out.println("Prijava uspjesna, user: " + usernameField);
-
+                Stage newStage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/panel.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
+                newStage.setTitle("Admin panel");
+                newStage.setScene(scene);
+                newStage.setResizable(false);
+                newStage.show();
+                Stage primaryStage = (Stage) usernameField.getScene().getWindow();
+                primaryStage.close();
             }
         }
     }
