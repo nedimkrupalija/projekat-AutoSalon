e48c5b5e3999df31b558008d5bdd9923581760fe
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-14 22:24:19 +0100

    implemented action to go back in panelController

--- src/main/java/ba/etf/unsa/rpr/controller/panelController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/panelController.java
@@ -8,8 +8,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class panelController {
 
@@ -29,6 +36,7 @@ public class panelController {
     public Label labelId;
     public Button categoryButton;
     public Button acceptButton;
+    public Button backButton;
 
     /**
      * Event on category pick
@@ -130,5 +138,23 @@ public class panelController {
 
     }
 
-
+    /**
+     * Action for returning to main screen from admin panel
+     * @param actionEvent
+     * @throws IOException
+     */
+    public void backButtonClick(ActionEvent actionEvent) throws IOException {
+        System.out.println("Vracanje na pocetni ekran!");
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
+        stage.setTitle("Login");
+        // scene.getStylesheets().add(String.valueOf(this.getClass().getResource("/css/style.css")));
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+        Stage thisStage = (Stage) labelId.getScene().getWindow();
+        thisStage.close();
+    }
 }
