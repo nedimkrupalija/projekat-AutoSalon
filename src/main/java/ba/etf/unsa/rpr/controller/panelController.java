8b58f5e15ea9a0849edecdf3ad9082f035a79cd6
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-14 01:18:15 +0100

    new design for admin panel

--- src/main/java/ba/etf/unsa/rpr/controller/panelController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/panelController.java
@@ -1,4 +1,21 @@
 package ba.etf.unsa.rpr.controller;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.stage.WindowEvent;
+
 public class panelController {
+
+    public Label labelUser;
+    public Label dateLabel;
+    public ChoiceBox picker;
+    public MenuButton menuButton;
+
+
+
+
+
+
 }
