7969b23d9559695130006dfd335ac748b3859fbd
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-15 15:59:27 +0100

    added listener for adding new cat.

--- src/main/java/ba/etf/unsa/rpr/controller/categoryController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/categoryController.java
@@ -1,5 +1,9 @@
 package ba.etf.unsa.rpr.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -12,4 +16,20 @@ public class categoryController {
 
 
     public Label categoryCount;
+    public TextField newCategoryName;
+    public Button acceptNewCategory;
+
+    @FXML
+    public void initialize(){
+        newCategoryName.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                if(newCategoryName.getText().trim().isEmpty()){
+                    newCategoryName.getStyleClass().add("wrongField");
+                }
+                else newCategoryName.getStyleClass().removeAll("wrongField");
+            }
+        });
+    }
+
 }
