646e2ead693b7d923ee6e9bc8b6860ad687d9aed
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 21:40:59 +0100

    added listener for username length in registrationController

---
+++ src/main/java/ba/etf/unsa/rpr/controller/registrationController.java
@@ -0,0 +1,41 @@
+package ba.etf.unsa.rpr.controller;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class registrationController {
+
+
+    public TextField nameTextField;
+    public Label nameLabel;
+    public TextField passTextField;
+    public Label passLabel;
+    public Button registerButton;
+    public Button backButton;
+
+    @FXML
+    public void initialize(){
+        nameLabel.setText("");
+        passLabel.setText("");
+        nameTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String newValue) {
+                if(newValue.trim().length()<3||newValue.trim().length()>10){
+                    nameLabel.setText("Ime treba biti duzine 3-10 karaktera!");
+                }
+                else nameLabel.setText("");
+            }
+        });
+    }
+
+    public void registerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void backButtonClick(ActionEvent actionEvent) {
+    }
+}
