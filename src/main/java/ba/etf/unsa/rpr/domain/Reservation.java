5129380c00cc80027a2d6c79ae07b8a64c4811e3
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 18:02:46 +0100

    implemented getter/setter methods for reservations

--- src/main/java/ba/etf/unsa/rpr/domain/Reservation.java
+++ src/main/java/ba/etf/unsa/rpr/domain/Reservation.java
@@ -16,4 +16,43 @@ public class Reservation {
     private User user;
     private Car car;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
