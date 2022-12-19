b8d68ce97db8c8286655934f4eeda6ddb716c44f
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 18:09:22 +0100

    implemented POJO for user

--- src/main/java/ba/etf/unsa/rpr/domain/User.java
+++ src/main/java/ba/etf/unsa/rpr/domain/User.java
@@ -4,7 +4,8 @@ import java.util.Objects;
 
 /**
  * @author Nedim Krupalija
- * Class that represents person selling some car
+ * Class for representing user with admin key within db
+ * POJO specification
  */
 
 
@@ -14,8 +15,58 @@ public class User {
     private String password;
     private int admin;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && admin == user.admin && Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password, admin);
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(int admin) {
+        this.admin = admin;
+    }
 }
