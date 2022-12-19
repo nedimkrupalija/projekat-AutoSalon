50ca979108751550f9fbd9582cfa59630d06c94f
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 18:08:20 +0100

    changed private fields of user / added admin validation within db field

---
+++ src/main/java/ba/etf/unsa/rpr/domain/User.java
@@ -0,0 +1,21 @@
+package ba.etf.unsa.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * @author Nedim Krupalija
+ * Class that represents person selling some car
+ */
+
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private int admin;
+
+
+
+
+
+}
