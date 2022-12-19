cd4d54055aff1d1e70b7b2c21e87c3bc718225c1
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 18:05:12 +0100

    changed car domain class / added private fields

---
+++ src/main/java/ba/etf/unsa/rpr/domain/Car.java
@@ -0,0 +1,20 @@
+package ba.etf.unsa.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * @author Nedim Krupalija
+ * Class for representing different cars
+ * POJO specification
+ */
+
+public class Car {
+    private int id;
+    private String name;
+    private String year;
+    private Reservation category;
+    private String color;
+    private int hP;
+
+
+}
