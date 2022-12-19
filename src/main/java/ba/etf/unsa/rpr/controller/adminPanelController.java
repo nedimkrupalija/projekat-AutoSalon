d892749ebce1fe8a8c86d01cf8581b2950aee2ba
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-20 00:13:18 +0100

    deleted adminPanelController and updated look

--- src/main/java/ba/etf/unsa/rpr/controller/adminPanelController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/adminPanelController.java
@@ -21,32 +21,17 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class adminPanelController {
 
     public Label labelUser;
-    public Label dateLabel;
 
-    public ChoiceBox picker;
+    public TextField textName;
 
-
-    public MenuButton menuButton;
-
-
-    public TextField textIme;
-    public TextField textTelefon;
     public TextField textPassword;
-    public TextField textPrezime;
+
 
     public Label labelId;
     public Button categoryButton;
     public Button acceptButton;
     public Button backButton;
 
-    /**
-     * Event on category pick
-     * @param mouseEvent
-     */
-    public void pickerClick(MouseEvent mouseEvent) {
-        if(picker.getSelectionModel().getSelectedItem()!=null)
-        categoryButton.setText("Idi na izmjenu: "+picker.getSelectionModel().getSelectedItem());
-    }
 
 
     /**
@@ -54,67 +39,11 @@ public class adminPanelController {
      * Sets css style if field is empty
      */
     @FXML
-    public void initialize(){
-        textIme.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                if(textIme.getText().trim().isEmpty()){
-                    textIme.getStyleClass().add("fieldWrong");
-                }
-                else textIme.getStyleClass().removeAll("fieldWrong");
-            }
-        });
-        textPrezime.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                if(textPrezime.getText().trim().isEmpty()){
-                    textPrezime.getStyleClass().add("fieldWrong");
-                }
-                else textPrezime.getStyleClass().removeAll("fieldWrong");
-            }
-        });
-        textTelefon.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                if(textTelefon.getText().trim().isEmpty()){
-                    textTelefon.getStyleClass().add("fieldWrong");
-                }
-                else textTelefon.getStyleClass().removeAll("fieldWrong");
-            }
-        });
-
-        textPassword.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                if(textPassword.getText().trim().isEmpty()){
-                    textPassword.getStyleClass().add("fieldWrong");
-                }
-                else textPassword.getStyleClass().removeAll("fieldWrong");
-            }
-        });
+    public void initialize() {
 
     }
 
-    /**
-     * Setting changed data for user
-     * @param user
-     */
-    private void setData(User user)
-    {
-        textIme.setText(user.getName());
-        textPrezime.setText(user.getName());
-        textPassword.setText(user.getPassword());
 
-    }
-
-    /**
-     * Action for changing data of salesman/user
-     * @param actionEvent
-     */
-    public void acceptClicked(ActionEvent actionEvent) throws SQLException {
-
-
-    }
 
     /**
      * Action for returning to main screen from admin panel
@@ -136,35 +65,4 @@ public class adminPanelController {
         thisStage.close();
     }
 
-    /**
-     * private method for setting table in category changer/updater
-     * @param loader
-     */
-    private void setCategoryChanger(FXMLLoader loader){
 
-    }
-
-    /**
-     * public action for going to category/car changer/updater
-     * @param actionEvent
-     * @throws IOException
-     */
-    public void categoryButtonClick(ActionEvent actionEvent) throws IOException {
-        if(picker.getSelectionModel().getSelectedItem().equals("Kategorija")){
-            System.out.println("Izmjena kategorije!");
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/kategorija.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
-            stage.setTitle("Kategorija");
-            stage.setScene(scene);
-            stage.setResizable(false);
-            setCategoryChanger(loader);
-            stage.showAndWait();
-        }
-        else if (picker.getSelectionModel().getSelectedItem().equals("Vozila")){
-
-        }
-        else return;
-    }
-}
