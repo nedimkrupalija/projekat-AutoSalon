d197b900eed6ef7609c855c235b44a69e904c43c
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 18:05:43 +0100

    implemented getter/setter methods for car

--- src/main/java/ba/etf/unsa/rpr/domain/Car.java
+++ src/main/java/ba/etf/unsa/rpr/domain/Car.java
@@ -12,9 +12,46 @@ public class Car {
     private int id;
     private String name;
     private String year;
-    private Reservation category;
     private String color;
     private int hP;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int gethP() {
+        return hP;
+    }
+
+    public void sethP(int hP) {
+        this.hP = hP;
+    }
 }
