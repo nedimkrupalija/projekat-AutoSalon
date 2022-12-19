45bb0a6467664b10e986511215acfd3e3673b1f4
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2022-12-19 20:04:48 +0100

    added initialize and listeners to fields

--- src/main/java/ba/etf/unsa/rpr/controller/loginController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/loginController.java
@@ -1,27 +1,20 @@
 package ba.etf.unsa.rpr.controller;
 
-import ba.etf.unsa.rpr.dao.Main;
-import ba.etf.unsa.rpr.dao.SalesmanDao;
-import ba.etf.unsa.rpr.dao.SalesmanDaoSQLImpl;
-import ba.etf.unsa.rpr.domain.Salesman;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import ba.etf.unsa.rpr.dao.UserDao;
+import ba.etf.unsa.rpr.dao.UserDaoSQLImpl;
+import ba.etf.unsa.rpr.domain.User;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Locale;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
@@ -31,6 +24,7 @@ public class loginController {
     /**
      * Private method to set css style of user field
      */
+
     private void setWrongUser(){
         usernameField.getStyleClass().removeAll("fieldWrong");
         usernameField.getStyleClass().add("fieldWrong");
@@ -39,6 +33,7 @@ public class loginController {
     /**
      * Private method to set css style of pass field
      */
+
     private void setWrongPass(){
         passwordField.getStyleClass().removeAll("fieldWrong");
         passwordField.getStyleClass().add("fieldWrong");
@@ -48,35 +43,26 @@ public class loginController {
     public PasswordField passwordField;
 
     /**
-     * Set all fields for next screen
-     * @param loader
+     * Initialize method / sets user and pass name red if they are empty
      */
-    private void setNextFields(FXMLLoader loader){
-        panelController panelController = loader.getController();
-        panelController.labelUser.setText(panelController.labelUser.getText()+" "+usernameField.getText());
-        LocalDate date = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.getDefault());
-        panelController.dateLabel.setText("Datum: " + date.format(formatter));
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Kategorija");
-        arrayList.add("Vozila");
-        ObservableList<String> observableList = FXCollections.observableArrayList(arrayList);
-        panelController.picker.setItems(observableList);
-    }
-
-    /**
-     * Method to set data for user that logged in
-     * @param loader
-     * @param salesman
-     */
-    private void setData(FXMLLoader loader, Salesman salesman)
-    {
-        panelController panelController = loader.getController();
-        panelController.labelId.setText(String.valueOf(salesman.getId()));
-        panelController.textIme.setText(salesman.getName());
-        panelController.textPrezime.setText(salesman.getSurname());
-        panelController.textTelefon.setText(salesman.getNumber());
-        panelController.textPassword.setText(salesman.getPassword());
+    @FXML
+    public void initialize(){
+        usernameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                if(newValue.trim().isEmpty()){
+                    setWrongUser();
+                }
+                else usernameField.getStyleClass().removeAll("fieldWrong");
+            }
+        });
+        passwordField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                if(t1.trim().isEmpty()) setWrongPass();
+                else passwordField.getStyleClass().removeAll("fieldWrong");
+            }
+        });
     }
 
 
@@ -85,38 +71,7 @@ public class loginController {
      * @param actionEvent btn click
      */
     public void loginClick(ActionEvent actionEvent) throws IOException {
-        if(passwordField.getText().isEmpty()){
-            setWrongPass();
-            System.out.println("Prazno polje za password.");
-        }
-        if(usernameField.getText().trim().isEmpty()) {
-            setWrongUser();
-            System.out.println("Prazan username");
-        }
-        else {
-            SalesmanDao salesmanDao = new SalesmanDaoSQLImpl();
-            Salesman salesman = salesmanDao.getByIdPass(Integer.parseInt(usernameField.getText()),passwordField.getText());
-            if(salesman==null) {
-                setWrongUser();
-                setWrongPass();
-                System.out.println("Prijava neuspjesna!");
-            }
-            else{
-                System.out.println("Prijava uspjesna, user: " + salesman.getName() + ", id: " + usernameField.getText());
-                Stage newStage = new Stage();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/panel.fxml"));
-                Parent root = loader.load();
-                setNextFields(loader);
-                setData(loader,salesman);
-                Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
-                newStage.setTitle("Admin panel");
-                newStage.setScene(scene);
-                //newStage.setResizable(false);
-                newStage.show();
-                Stage primaryStage = (Stage) usernameField.getScene().getWindow();
-                primaryStage.close();
-            }
-        }
+
     }
 
     /**
