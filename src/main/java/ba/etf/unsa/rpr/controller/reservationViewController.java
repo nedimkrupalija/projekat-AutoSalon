261fb84488a0ca762402447b7b1eb89a01fded85
nedimkrupalija <krupalijanedim@gmail.com>
2022-12-21 15:12:00 +0100

    implemented action for going back to admin panel

--- src/main/java/ba/etf/unsa/rpr/controller/reservationViewController.java
+++ src/main/java/ba/etf/unsa/rpr/controller/reservationViewController.java
@@ -1,4 +1,49 @@
 package ba.etf.unsa.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class reservationViewController {
+
+    public ListView reservationList;
+    public Label idLabel;
+    public Label labelDate;
+
+    public DatePicker pickerArrivalDate;
+    public Button changeDateButton;
+    public Label labelCar;
+    public Label labelUser;
+    public Button deleteButton;
+    public Button backButton;
+
+    /**
+     * Action for going back to main admin panel
+     * @param actionEvent
+     */
+    public void backButtonClick(ActionEvent actionEvent) throws IOException {
+        System.out.println("Nazad na admin panel!");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminPanel.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
+        stage.setScene(scene);
+        Stage currentStage = (Stage) idLabel.getScene().getWindow();
+        stage.show();
+        currentStage.close();
+    }
+
+    public void deleteButtonClick(ActionEvent actionEvent) {
+    }
 }
