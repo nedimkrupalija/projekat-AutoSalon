69ed889e3814639c5ffed76ba3cc3f2f9f424e25
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-02-08 16:07:47 +0100

    tests for reservations

---
+++ src/test/java/ba/etf/unsa/rpr/business/ReservationManagerTest.java
@@ -0,0 +1,23 @@
+package ba.etf.unsa.rpr.business;
+
+import ba.etf.unsa.rpr.domain.Reservation;
+import ba.etf.unsa.rpr.exception.ReservationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationManagerTest {
+
+    @Test
+    void getAll() {
+        assertDoesNotThrow(()->{new ReservationManager().getAll();});
+    }
+
+
+
+
+    @Test
+    void getByid() {
+        assertThrows(ReservationException.class, ()->{new ReservationManager().getByid(99999);});
+    }
+}
